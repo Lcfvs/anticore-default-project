6eca673b84966305fbd1e669e58f369640e9b3f0
Lcf.vs <lcf.vs@hotmail.com>
2019-01-20 14:22:28 +0100

    Update default.md.js

--- assets/js/dev/default.md.js
+++ assets/js/dev/default.md.js
@@ -2,7 +2,7 @@ import {anticore} from 'anticore'
 import {one} from 'anticore/dom/query/one'
 import {replace} from 'anticore/dom/tree/replace'
 
-anticore.on('.default', function(element, next) {
+anticore.on('.default', function(element, next, loaded) {
   loaded && replace(element, one('main'))
   next()
 })
